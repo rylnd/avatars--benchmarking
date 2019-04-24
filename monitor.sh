628566f785039379bd87b57a2f3ed383e862e007
Ryland Herrick <ryalnd@gmail.com>
2019-04-24 11:50:39 -0500

    Poll as often as possible
    Not just at 1s intervals.

--- monitor.sh
+++ monitor.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-psrecord $(pgrep node | head -n1) --interval 1 --log psrecord.txt --plot plot.png  --include-children
+psrecord $(pgrep node | head -n1) --log monitor.txt --plot plot.png --include-children
